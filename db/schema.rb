e12fa934c9abb69f1984a5b9dc67ddc8cd5c9040
MrOrz <johnsonliang7@gmail.com>
2012-05-10 01:35:40 +0800

    `rake db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120509171038) do
+ActiveRecord::Schema.define(:version => 20120509173410) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -30,5 +30,14 @@ ActiveRecord::Schema.define(:version => 20120509171038) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
 end
