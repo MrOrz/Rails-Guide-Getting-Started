0e141f666e4ed55aed54a5e56d1a7373363e2fa2
MrOrz <johnsonliang7@gmail.com>
2012-05-10 01:13:29 +0800

    7.2 Associating Models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
+
+  has_many :comments
+
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
