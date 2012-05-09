0308611bcf66fca2f50d0b35c127c69967e5c70d
MrOrz <johnsonliang7@gmail.com>
2012-05-10 01:56:02 +0800

    finish "11 Building a Multi-Model Form"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
+  attr_accessible :content, :name, :title, :tags_attributes
 
   has_many :comments, :dependent => :destroy
+  has_many :tags
 
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
