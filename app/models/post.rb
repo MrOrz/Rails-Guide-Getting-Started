c88a43cca01f0d932a49437fbc7db39935e351b8
MrOrz <johnsonliang7@gmail.com>
2012-05-10 00:49:36 +0800

    6.5 Adding Some Validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
 end
